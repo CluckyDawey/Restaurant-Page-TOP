bf92b8a7a7a702c4b9b2d8ee11d1fcf6c3ac220f
Andre Bundal <andrecarlosbundal@gmail.com>
2026-05-05 16:01:21 +0800

    Created an initial template for menu part of the webpage

--- src/index.js
+++ src/index.js
@@ -34,6 +34,61 @@ import carbonara from "./image_341fbfb5.png";
     let menuContent = document.createElement("div");
     menuContent.classList.add("menu-content");
 
+
+    // Could be sidebar or tabs
+    let menuSelection = document.createElement("div");
+    menuSelection.classList.add("menu-selection");
+    let mainCourse = document.createElement("div");
+    mainCourse.classList.add("menu-category");
+    let mainCourseTitle = document.createElement("h2");
+    mainCourseTitle.textContent = "Main Course";
+    mainCourse.appendChild(mainCourseTitle);
+
+    let sideDishes = document.createElement("div");
+    sideDishes.classList.add("menu-category");
+    let sideDishesTitle = document.createElement("h2");
+    sideDishesTitle.textContent = "Side Dishes";
+    sideDishes.appendChild(sideDishesTitle);
+
+    let desserts = document.createElement("div");
+    desserts.classList.add("menu-category");
+    let dessertsTitle = document.createElement("h2");
+    dessertsTitle.textContent = "Desserts";
+    desserts.appendChild(dessertsTitle);
+    
+    let drinks = document.createElement("div");
+    drinks.classList.add("menu-category");
+    let drinksTitle = document.createElement("h2");
+    drinksTitle.textContent = "Drinks";
+    drinks.appendChild(drinksTitle);
+
+    menuSelection.appendChild(mainCourse);
+    menuSelection.appendChild(sideDishes);
+    menuSelection.appendChild(desserts);
+    menuSelection.appendChild(drinks);
+
+    // display area for menu items
+    let menuDisplay = document.createElement("div");
+    menuDisplay.classList.add("menu-display");
+
+    let mainCourseItems = document.createElement("div");
+    mainCourseItems.classList.add("menu-items");
+    let mainCourseItem1 = document.createElement("div");
+    mainCourseItem1.classList.add("menu-item");
+    let mainCourseItem1Title = document.createElement("h3");
+    mainCourseItem1Title.textContent = "Spaghetti";
+    let mainCourseItem1Description = document.createElement("p");
+    mainCourseItem1Description.textContent = "Classic spaghetti with tomato sauce and basil.";
+    mainCourseItem1.appendChild(mainCourseItem1Title);
+    mainCourseItem1.appendChild(mainCourseItem1Description);
+    mainCourseItems.appendChild(mainCourseItem1);
+    menuDisplay.appendChild(mainCourseItems);
+
+
+    
+    menuContent.appendChild(menuSelection);
+    menuContent.appendChild(menuDisplay);
+    menu.appendChild(menuContent);
     content.appendChild(menu);
 
     let contact = document.createElement("div");
